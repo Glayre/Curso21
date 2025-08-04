a6849c784001b7b3dfe96e469c6ef95c76977985
Glayre <glayrecalles@gmail.com>
2025-08-03 23:39:42 -0400

    actualizacion react

--- 6-react/src/main.jsx
+++ 6-react/src/main.jsx
@@ -1,10 +1,6 @@
+import Button from "./src/components/Button.jsx"; 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import Button from './components/Button.jsx';
-
-
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
